ad4cf45be44bb60f68ab3a5876f7cda134206783
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-03-05 18:19:23 +0200

    startup DB values

--- db/common.sql
+++ db/common.sql
@@ -1,4 +1,4 @@
-CREATE SEQUENCE "language_id";
+CREATE SEQUENCE "language_id" START WITH 10;
 CREATE TABLE "language" (
     "id" INTEGER NOT NULL default nextval('language_id'),
     "name" CHARACTER VARYING(16) NOT NULL,
@@ -7,8 +7,8 @@ CREATE TABLE "language" (
     "active" BOOLEAN NOT NULL,
     PRIMARY KEY("id")
 );
-insert into "language" (name) values ('English', 'en', 'English', true);
-insert into "language" (name) values ('Russian', 'ru', 'Русский', true);
+insert into "language" ("id", "name", "code", "native") values (1, 'English', 'en', 'English', true);
+insert into "language" ("id", "name", "code", "native") values (2, 'Russian', 'ru', 'Русский', true);
 
 
 CREATE SEQUENCE "unit_id";
@@ -18,7 +18,7 @@ CREATE TABLE "unit" (
     "sign" CHARACTER VARYING(16) NULL,
     PRIMARY KEY("id")
 );
-CREATE SEQUENCE "unit_i18n_id";
+CREATE SEQUENCE "unit_i18n_id" START WITH 10;
 CREATE TABLE "unit_i18n" (
     "id" BIGINT NOT NULL default nextval('unit_i18n_id'),
     "object_id" INTEGER NOT NULL REFERENCES "unit"("id") ON UPDATE CASCADE ON DELETE CASCADE,
@@ -30,9 +30,14 @@ CREATE INDEX unit_i18n_object_id_idx ON unit_i18n(object_id);
 CREATE INDEX unit_i18n_language_id_idx ON unit_i18n(language_id);
 CREATE UNIQUE INDEX unit_i18n_object_id_language_id_uidx ON "unit_i18n"("object_id", "language_id");
 
+insert into "unit" ("id", "name", "sign") values (1, 'money', '&euro;');
+insert into "unit" ("id", "name", "sign") values (2, 'quantity', '');
+insert into "unit" ("id", "name", "sign") values (3, 'flag', '');	-- all internal options
+insert into "unit_i18n" ("object_id", "language_id", "name") select "id", 1, "name" from "unit";
+
 
 CREATE SEQUENCE "city_id";
-CREATE TABLE "city" (
+CREATE TABLE "city" START WITH 10 (
     "id" INTEGER NOT NULL default nextval('unit_id'),
     "name" CHARACTER VARYING(16) NULL,
     "latitude" numeric(10,6) NULL,
@@ -51,3 +56,9 @@ CREATE TABLE "city_i18n" (
 CREATE INDEX city_i18n_object_id_idx ON city_i18n(object_id);
 CREATE INDEX city_i18n_language_id_idx ON city_i18n(language_id);
 CREATE UNIQUE INDEX city_i18n_object_id_language_id_uidx ON "city_i18n"("object_id", "language_id");
+
+insert into "city" ("id", "name", "latitude", "longitude") values (1, 'Nicosia', 0, 0);
+insert into "city" ("id", "name", "latitude", "longitude") values (2, 'Limassol', 0, 0);
+insert into "city" ("id", "name", "latitude", "longitude") values (3, 'Larnaka', 0, 0);
+insert into "city" ("id", "name", "latitude", "longitude") values (4, 'Paphos', 0, 0);
+insert into "city_i18n" ("object_id", "language_id", "name") select "id", 1, "name" from "city";
