a6d8d533456f7983b42a591447700eb326664b7a
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-04-09 17:26:09 +0300

    editor bugfix

--- src/admin/webroot/js/wysiwyg-html5.0.3.0.js
+++ src/admin/webroot/js/wysiwyg-html5.0.3.0.js
@@ -7665,8 +7665,26 @@ wysihtml5.Commands = Base.extend(
   var undef,
       CLASS_NAME  = "wysiwyg-text-align-right",
       REG_EXP     = /wysiwyg-text-align-[a-z]+/g;
-  
+
   wysihtml5.commands.justifyRight = {
+    exec: function(composer, command) {
+      return wysihtml5.commands.formatBlock.exec(composer, "formatBlock", null, CLASS_NAME, REG_EXP);
+    },
+
+    state: function(composer, command) {
+      return wysihtml5.commands.formatBlock.state(composer, "formatBlock", null, CLASS_NAME, REG_EXP);
+    },
+
+    value: function() {
+      return undef;
+    }
+  };
+})(wysihtml5);(function(wysihtml5) {
+  var undef,
+      CLASS_NAME  = "wysiwyg-text-align-justify",
+      REG_EXP     = /wysiwyg-text-align-[a-z]+/g;
+
+  wysihtml5.commands.justifyFull = {
     exec: function(composer, command) {
       return wysihtml5.commands.formatBlock.exec(composer, "formatBlock", null, CLASS_NAME, REG_EXP);
     },
