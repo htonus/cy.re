f69714f25435e161bdfcff433cd28bc842f96b5b
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-06-21 16:39:07 +0300

    nice title

--- src/user/views/buy/index.tpl.php
+++ src/user/views/buy/index.tpl.php
@@ -11,8 +11,18 @@
 
 				<div class="span4">
 					<div class="catchy">
-						<h2><strong>WELCOME!</strong></h2>
-						<h5 style="color: #fff;">Your most important message or cacthy phrase goes here and also it goes right here.</h5>
+						<div class="pull-right white" style="text-align: right"><b>Register</b><br>Forgot password</div>
+						<h2><strong>WELCOME</strong></h2>
+						<div class="row-fluid form-inline">
+							<div class="span6"><input type="text" name="username" class="input-block-level" /></div>
+							<div class="span3"><input type="password" name="userpass" class="input-block-level" /></div>
+							<div class="span3"><a href="#" class="btn input-block-level">Submit</a></div>
+						</div>
+						<div class="row-fluid">
+							<div class="span6 white">User name</div>
+							<div class="span3 white">Password</div>
+							<div class="span3 white"></div>
+						</div>
 					</div>
 
 					<p class="intro">Simple and flexible HTML, CSS, and Javascript for popular user interface components and interactions.</p>
