62209630eb92f2c82be72b9760b11dadb31d1432
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-08-29 17:47:21 +0300

    price in context of the offer type

--- src/user/views/rent/index.tpl.php
+++ src/user/views/rent/index.tpl.php
@@ -33,48 +33,11 @@
 
 	$partViewer->view(
 		'_parts/blocks/carousel',
-		Model::create()->set('list', $blocks[CustomType::CAROUSEL])
+		Model::create()->
+			set('list', $blocks[CustomType::CAROUSEL])->
+			set('area', $area)
 	);
 ?>
-				</div>
-
-			</div>
-		</div>
-
-		<div class="container latest">
-			<div class="row">
-
-				<div class="span4">
-					<div class="block">
-						<h4>
-							<img title="round1" src="http://html.orange-idea.com/commander/wp-content/uploads/2012/09/admin.png" alt="" width="25" height="25">
-							Powerful Admin Panel
-						</h4>
-						<div class="clearfix"></div>
-						<p>We have built a custom options panel with more then 300 options, that let’s you customize your theme any way you want to!</p>
-					</div>
-				</div>
-
-				<div class="span4">
-					<div class="block">
-						<img title="round1" src="http://html.orange-idea.com/commander/wp-content/uploads/2012/09/frame.png" alt="" width="25" height="25" class="pull-left">
-						<h4 style="text-align: center;">
-							Page Builder Included
-						</h4>
-						<div class="clearfix"></div>
-						<p>Now you’ll be able to create complex layouts within minutes! It’s build on top of the Twitter Bootstrap and jQuery UI frameworks!</p>
-					</div>
-				</div>
-
-				<div class="span4">
-					<div class="block">
-						<img title="round1" src="http://html.orange-idea.com/commander/wp-content/uploads/2012/09/ipad.png" alt="" width="25" height="25" class="pull-right">
-						<h4>
-							Responsive Design
-						</h4>
-						<div class="clearfix"></div>
-						<p>Responsive designs that addapts to smaller devices. Also Builder Theme have 4 loayouts: 2 Boxed and 2 Fullwidth!</p>
-					</div>
 				</div>
 
 			</div>
@@ -82,13 +45,20 @@
 
 
 <?php
+
+	$partViewer->view(
+		'_parts/blocks/latest',
+		Model::create()->
+			set('list', empty($latestList) ? array() : $latestList)
+	);
+
 	$partViewer->view(
 		'_parts/blocks/recent',
 		Model::create()->
 			set('area', $area)->
 			set('list', $blocks[CustomType::RECENT])
 	);
-	
+
 	$partViewer->view(
 		'_parts/blocks/about',
 		Model::create()->
@@ -96,5 +66,4 @@
 			set('item', $static[StaticType::COMPANY])
 	);
 ?>
-
 	</section>
