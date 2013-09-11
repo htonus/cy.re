b223714b2b18116f06a900163f6fe3b787fb04c0
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-09-11 16:46:40 +0300

    article to realty linking editor

--- src/admin/views/article/edit.tpl.php
+++ src/admin/views/article/edit.tpl.php
@@ -45,8 +45,9 @@
 		<ul class="nav nav-tabs">
 			<li<?= $id ? null : ' class="active"'?>><a href="#tab_description" data-toggle="tab">Description</a></li>
 			<li<?= $id ? ' class="active"' : null?>><a href="#tab_pictures" data-toggle="tab">Pictures</a></li>
+			<li><a href="#tab_sites" data-toggle="tab">Branded realty</a></li>
 		</ul>
-		<div class="controls pull-right">
+		<div class="pull-right">
 			<button class="btn" type="button" onclick="document.location.href='/index.php?area=<?= $area?>'">Cancel</button>
 			<button class="btn btn-primary" type="submit" id="btnSubmit">Submit</button>
 <?php
@@ -109,6 +110,108 @@
 ?>
 	</div>
 
+
+	<div class="tab-pane" id="tab_sites">
+
+		<table class="table" id="itemList">
+		<tbody>
+<?php
+	$total = 0;
+
+	if (
+		$form->getValue('id')
+		&& ($list = $form->getValue('id')->getSites()->getList())
+	) {
+		foreach ($list as $item) {
+			$total ++;
+			$realty = $item;
+?>
+			<tr>
+				<td><img src="<?= PictureSize::thumbnail()->getUrl($realty->getPreview())?>"></td>
+				<td>
+					<b><?= $realty->getCode()?></b> : <?= $realty->getName()?>
+				</td>
+				<td style="text-align: right">
+					<input type="hidden" name="sites[]" value="<?= $id?>"/>
+					<a href="/index.php?area=realty&action=edit&id=<?= $realty->getId()?>" target="_blank" class="btn btn-info">View</a>
+					<button type="button" class="btn btn-warning" onclick="jq(this).parents('TR').remove()">Remove</button>
+				</td>
+			</tr>
+<?
+		}
+	} else {
+?>
+			<tr id="emptyList">
+				<td colspan="3">Empty list</td>
+			</tr>
+<?php
+	}
+?>
+		</tbody>
+		</table>
+
+		<div class="navbar">
+			<div class="navbar-inner">
+				<div class="container">
+					<a class="brand" href="#">Realty search</a>
+
+					<ul class="nav">
+						<li><a href="#">Enter ID or CODE</a></li>
+						<li class="navbar-search">
+							<input type="text" id="id_or_code" name="id_or_code" class="search-query span2" placeholder="Search">
+						</li>
+						<li>&nbsp;</li>
+						<li>
+							<input type="button" id="searchButton" name="input_id_or_code" value="Attach" class="btn">
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	
 </form>
 
 </div>
+
+<script type="text/javascript">
+var itemListTotal = <?= $total?>;
+
+jq(document).ready(function(){
+
+	jq('.moveup').click(itemMoveUp);
+
+	jq('.movedown').click(itemMoveDown);
+
+	jq('#searchButton').click(function(){
+		var criteria = jq('#id_or_code').val();
+
+		if (criteria.length < 1)
+			return;
+
+		jq.getJSON(
+			'/index.php?area=custom&action=searchRealty&criteria=' + criteria,
+			function(data){
+				if (data.error == '') {
+					itemListTotal ++;
+					jq('#emptyList').remove();
+					jq('#itemList TBODY').append('\
+			<tr>\
+				<td style="width: <?= PictureSize::thumbnail()->getWidth()?>px"><img src="' + data.item.url+ '"></td>\
+				<td><b>' + data.item.realty_code + '</b> : ' + data.item.name + '</td>\
+				<td style="text-align: right">\
+					<input type="hidden" name="sites[]" value="' + data.item.realty_id + '"/>\
+					<a href="/index.php?area=realty&action=edit&id=' + data.item.realty_id + '" target="_blank" class="btn btn-info">View</a>\
+					<a href="/index.php?area=realty&action=edit&id=' + data.item.realty_id + '" target="_blank" class="btn btn-warning">Remove</a>\
+				</td>\
+			</tr>\
+					');
+				} else {
+					alert(data.error);
+				}
+			}
+		);
+	});
+});
+</script>
