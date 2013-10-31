b9852ee1ccec79573c384b104816b0008ff48a88
Mike <htonus@gmail.com>
2013-10-31 19:09:34 +0200

    layout for about

---
+++ src/user/views/about/layout.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * $Id$
+ */
+
+        $partViewer->view('_parts/site_title');
+
+?>
+
+<div class="content sub-page">
+
+<?php
+        $partViewer->view("$area/$action");
+
+        $partViewer->view('_parts/site_footer');
+?>
+
+</div>
