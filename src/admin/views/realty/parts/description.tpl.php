a03837c5900d7def9cba8d51a083f4b9f363db04
Mikhail Cherviakov <m.cherviakov@fxpro.com>
2013-11-08 14:39:48 +0200

    map to help set points

--- src/admin/views/realty/parts/description.tpl.php
+++ src/admin/views/realty/parts/description.tpl.php
@@ -27,10 +27,12 @@
 			</div>
 		</div>
 
+
 		<div class="control-group">
-			<label class="control-label" for="input_longitude">Longitude</label>
+			<label class="control-label" for="input_longitude">Plot on map</label>
 			<div class="controls">
-				<input type="text" id="input_longitude" placeholder="longitude" name="longitude" value="<?= $form->getValue('longitude')?>" />
+				<input type="checkbox" name="polygon" value="<?= $form->getValue('polygon') ?>" <?= $form->getValue('polygon') ? 'checked="checked"' : null?> />
+				<input type="button" id="setLocation" value="Choose on Map" class="btn btn-warning btn-small" />
 			</div>
 		</div>
 
@@ -38,6 +40,13 @@
 			<label class="control-label" for="input_latitude">Latitude</label>
 			<div class="controls">
 				<input type="text" id="input_latitude" placeholder="latitude" name="latitude" value="<?= $form->getValue('latitude')?>" />
+			</div>
+		</div>
+		
+		<div class="control-group">
+			<label class="control-label" for="input_longitude">Longitude</label>
+			<div class="controls">
+				<input type="text" id="input_longitude" placeholder="longitude" name="longitude" value="<?= $form->getValue('longitude')?>" />
 			</div>
 		</div>
 
@@ -229,3 +238,142 @@ function updateSelector(name, list)
 	$model->set('editorFor', 'text');
 	$partViewer->view('_parts/form/i18n');
 
+?>
+
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&libraries=drawing"></script>
+<script type="text/javascript">
+var mapZoom = 9;
+var cyprus = new google.maps.LatLng(34.691739, 33.028203);
+var city = <?= $form->getValue('city') ? 'new google.maps.LatLng('.$form->getValue('city')->getLatitude().', '.$form->getValue('city')->getLongitude().')' : 'null' ?>;
+var marker = null;
+var polygon = null;
+
+jq(document).ready(function(){
+	jq('#setLocation').click(function(){
+		var center = cyprus;
+		
+		if (jq('#input_latitude').val() && jq('#input_longitude').val()) {
+			center = new google.maps.LatLng(jq('#input_latitude').val(), jq('#input_longitude').val());
+			mapZoom = 15;
+			marker = new google.maps.Marker({position: center, draggable: true, animation: google.maps.Animation.DROP, zIndex: 1000});
+		} else if (city) {
+			center = city;
+			mapZoom = 12;
+		}
+
+		var mapOptions = {
+			center: center,
+			zoom: mapZoom,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+
+		jq('#locationModal')
+			.modal('show')
+			.on('shown', function(){
+
+			var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+			if (marker)
+				marker.setMap(map);
+			
+			google.maps.event.addListener(marker, 'dragend', function(e) {
+				map.panTo(e.latLng);
+				jq('#input_latitude').val(e.latLng.lat());
+				jq('#input_longitude').val(e.latLng.lng());
+			});
+
+			var drawingManager = new google.maps.drawing.DrawingManager({
+				drawingMode: null //google.maps.drawing.OverlayType.MARKER,
+			,	drawingControl: true
+			,	drawingControlOptions: {
+					position: google.maps.ControlPosition.TOP_CENTER,
+					drawingModes: [
+						google.maps.drawing.OverlayType.MARKER
+//					,	google.maps.drawing.OverlayType.CIRCLE
+					,	google.maps.drawing.OverlayType.POLYGON
+//					,	google.maps.drawing.OverlayType.POLYLINE
+//					,	google.maps.drawing.OverlayType.RECTANGLE
+					]
+				}
+			,	polygonOptions: {
+					editable: true
+				,	fillColor: '#0C0'
+				,	fillOpacity: 0.2
+				,	strokeColor: '#090'
+				,	strokeOpacity: 0.5
+				,	strokeWeight: 2
+				}
+			,	markerOptions: {
+					draggable:true
+				,	animation: google.maps.Animation.DROP
+				,	zIndex: 1000
+				}
+			,	circleOptions: {
+					fillColor: '#ffff00'
+				,	fillOpacity: 1
+				,	strokeWeight: 5
+				,	clickable: false
+				,	editable: true
+				,	zIndex: 1
+				}
+			});
+			
+			drawingManager.setMap(map);
+			
+			google.maps.event.addListener(drawingManager, 'markercomplete', function(newMarker){
+				if (marker) {
+					marker.setMap(null);
+				}
+
+				marker = newMarker;
+				map.panTo(marker.getPosition());
+				jq('#input_latitude').val(marker.getPosition().lat());
+				jq('#input_longitude').val(marker.getPosition().lng());
+				drawingManager.setDrawingMode(null);
+				google.maps.event.addListener(marker, 'dragend', function(e) {
+					map.panTo(e.latLng);
+					jq('#input_latitude').val(e.latLng.lat());
+					jq('#input_longitude').val(e.latLng.lng());
+				});
+			});
+
+			google.maps.event.addListener(drawingManager, 'polygoncomplete', function(newPolygon) {
+				if (polygon) {
+					polygon.setMap(null);
+				}
+
+				polygon = newPolygon;
+				drawingManager.setDrawingMode(null);
+//				jq('#input_polygon').val(polygon.getPosition().lng());
+			});
+		});
+	});
+});
+
+function clearMap()
+{
+	if (marker)
+		marker.setMap(null);
+	if (polygon)
+		polygon.setMap(null);
+
+	jq('#input_latitude').val('');
+	jq('#input_longitude').val('');
+	jq('#input_polygon').val('');
+}
+</script>
+
+
+<div class="modal hide fade" id="locationModal">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h3>Modal header</h3>
+	</div>
+	<div class="modal-body">
+		<div id="googleMap" style="width: 100%; height: 400px;"></div>
+	</div>
+	<div class="modal-footer">
+		<a href="#" onclick="clearMap()" class="btn btn-danger">Clear map</a>
+		<a href="#" data-dismiss="modal" class="btn">Close</a>
+	</div>
+</div>
